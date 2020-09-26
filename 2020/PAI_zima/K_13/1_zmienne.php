5515aa145714f2e57b34e9eade6e472ee7ceac71
Piotr Michalak <piotrmichalak1997@gmail.com>
2020-09-26 15:01:38 +0200

    wsb1

--- 2020/PAI_zima/K_13/1_zmienne.php
+++ 2020/PAI_zima/K_13/1_zmienne.php
@@ -7,5 +7,51 @@
 </head>
 <body>
     Pierwszy plik<hr>
+    <?php
+        $name='Anna';
+        echo 'Imię: $name<br>';
+        echo "Imię: $name<br>";
+        // konkatenacja
+        echo 'WSB'.'<hr>';
+
+        // składnia heredoc
+        $text = <<<LABEL
+            Heredoc<br>
+            Imię: $name<br>
+            <hr>
+        LABEL;
+
+        echo $text;
+
+        echo <<<L
+            Heredoc 2<br>
+            Imię: $name
+            <hr>
+        L;
+
+        // składnia nowdoc
+        echo <<<'L'
+            Nowdoc 2<br>
+            Imię: $name
+            <hr>
+        L;
+
+        // integer
+            $bin = 0b1010;
+            $oct = 011;
+            $hex = 0x11;
+            echo $hex,'<hr>';
+
+        $city='Poznań';
+        echo "Nazwa zmiennej: $city, wartość: $city";
+
+        $x = true;
+        $x = false;
+        echo $x,'<br>';
+
+        // wersja PHP
+        echo PHP_VERSION; //7.4.10
+        echo 
+    ?>
 </body>
 <head>
